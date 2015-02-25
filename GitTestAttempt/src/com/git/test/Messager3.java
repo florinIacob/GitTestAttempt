450929655c59ca05393d15285a8bd73c4f6e223a
florinIacob <florin.e.iacob@gmail.com>
2015-02-25 14:20:44 +0200

    Update and rename GitTestAttempt/src/com/git/test to GitTestAttempt/src/com/git/test/Messager3.java

---
+++ GitTestAttempt/src/com/git/test/Messager3.java
@@ -0,0 +1,9 @@
+package com.git.test;
+
+public class Messager3 {
+
+	public static void printMessage(String message) {
+		System.out.println(" >>Messager3<< The message is : " + message);
+	}
+	
+}
